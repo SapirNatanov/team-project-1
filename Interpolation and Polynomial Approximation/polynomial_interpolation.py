71dd1a56ecbda5bcaf145550ba595c4b62c28fa2
Dor Maudi <157501429+DorMaudi@users.noreply.github.com>
2024-04-08 09:36:11 +0300

    Update polynomial_interpolation.py

--- Interpolation and Polynomial Approximation/polynomial_interpolation.py
+++ Interpolation and Polynomial Approximation/polynomial_interpolation.py
@@ -2,102 +2,11 @@ from colors import bcolors
 from matrix_utility import *
 
 
-def GaussJordanElimination(matrix, vector):
-    """
-    Function for solving a linear equation using gauss's elimination method
-    :param matrix: Matrix nxn
-    :param vector: Vector n
-    :return: Solve Ax=b -> x=A(-1)b
-    """
-    # Pivoting process
-    matrix, vector = RowXchange(matrix, vector)
-    # Inverse matrix calculation
-    invert = InverseMatrix(matrix, vector)
-    return MulMatrixVector(invert, vector)
-
-
-def UMatrix(matrix, vector):
-    """
-    :param matrix: Matrix nxn
-    :return:Disassembly into a  U matrix
-    """
-    # result matrix initialized as singularity matrix
-    U = MakeIMatrix(len(matrix), len(matrix))
-    # loop for each row
-    for i in range(len(matrix[0])):
-        # pivoting process
-        matrix, vector = RowXchageZero(matrix, vector)
-        for j in range(i + 1, len(matrix)):
-            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
-            # Finding the M(ij) to reset the organs under the pivot
-            elementary[j][i] = -(matrix[j][i]) / matrix[i][i]
-            matrix = MultiplyMatrix(elementary, matrix)
-    # U matrix is a doubling of elementary matrices that we used to reset organs under the pivot
-    U = MultiplyMatrix(U, matrix)
-    return U
-
-
-def LMatrix(matrix, vector):
-    """
-       :param matrix: Matrix nxn
-       :return:Disassembly into a  L matrix
-       """
-    # Initialize the result matrix
-    L = MakeIMatrix(len(matrix), len(matrix))
-    # loop for each row
-    for i in range(len(matrix[0])):
-        # pivoting process
-        matrix, vector = RowXchageZero(matrix, vector)
-        for j in range(i + 1, len(matrix)):
-            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
-            # Finding the M(ij) to reset the organs under the pivot
-            elementary[j][i] = -(matrix[j][i]) / matrix[i][i]
-            # L matrix is a doubling of inverse elementary matrices
-            L[j][i] = (matrix[j][i]) / matrix[i][i]
-            matrix = MultiplyMatrix(elementary, matrix)
-
-    return L
-
-
-def SolveLU(matrix, vector):
-    """
-    Function for deconstructing a linear equation by ungrouping LU
-    :param matrix: Matrix nxn
-    :param vector: Vector n
-    :return: Solve Ax=b -> x=U(-1)L(-1)b
-    """
-    matrixU = UMatrix(matrix)
-    matrixL = LMatrix(matrix)
-    return MultiplyMatrix(InverseMatrix(matrixU), MultiplyMatrix(InverseMatrix(matrixL), vector))
-
-
-def solveMatrix(matrixA, vectorb):
-    detA = Determinant(matrixA, 1)
-    print(bcolors.YELLOW, "\nDET(A) = ", detA)
-
-    if detA != 0:
-        print("CondA = ", Cond(matrixA, InverseMatrix(matrixA, vectorb)), bcolors.ENDC)
-        print(bcolors.OKBLUE, "\nnon-Singular Matrix - Perform GaussJordanElimination", bcolors.ENDC)
-        result = GaussJordanElimination(matrixA, vectorb)
-        print(np.array(result))
-        return result
-    else:
-        print("Singular Matrix - Perform LU Decomposition\n")
-        print("Matrix U: \n")
-        print(np.array(UMatrix(matrixA, vectorb)))
-        print("\nMatrix L: \n")
-        print(np.array(LMatrix(matrixA, vectorb)))
-        print("\nMatrix A=LU: \n")
-        result = MultiplyMatrix(LMatrix(matrixA, vectorb), UMatrix(matrixA, vectorb))
-        print(np.array(result))
-        return result
-
-
 def polynomialInterpolation(table_points, x):
     x_data = [point[0] for point in table_points]
     y_data = [point[1] for point in table_points]
 
-    # Perform polynomial interpolation using numpy.polyfit
+    # Perform polynomial interpolation using numpy. poly fit
     coefficients = np.polyfit(x_data, y_data, deg=len(x_data) - 1)
 
     # Construct the polynomial function
@@ -119,8 +28,8 @@ def polynomialInterpolation(table_points, x):
 
 
 if __name__ == '__main__':
-    table_points = [(0, 0), (1, 0.8415), (2, 0.9093), (3, 0.1411), (4, -0.7568), (5, -0.9589), (6, -0.2794)]
-    x = 2.6
+    table_points = [(-3.27, -2), (-1.52, 30.2), (4.2, 2.9093), (10.156, -40)]
+    x = 0.2
     print(bcolors.OKBLUE, "----------------- Interpolation & Extrapolation Methods -----------------\n", bcolors.ENDC)
     print(bcolors.OKBLUE, "Table Points: ", bcolors.ENDC, table_points)
     print(bcolors.OKBLUE, "Finding an approximation to the point: ", bcolors.ENDC, x, '\n')
